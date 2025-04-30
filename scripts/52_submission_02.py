457a387e373025210e4faad1fd33b505467dc48b
VitusPuttmann <vitus.puttmann@outlook.com>
2025-05-01 00:22:11 +0200

    Update script formatting

--- scripts/52_submission_02.py
+++ scripts/52_submission_02.py
@@ -17,6 +17,7 @@
 ##                                                                          ##
 ## Import libraries
 
+import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
